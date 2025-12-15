993aef0e706d9e612d287b632a4ece59e2070165
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-12-14 20:13:28 -0600

    Automatically login to Tailscale
    This uses an OAuth flow with the API to generate a machine auth key only
    if necessary. This has the advantage of never expiring and enabling the
    tags to be programatically set

--- pi-hole-configuration.nix
+++ pi-hole-configuration.nix
@@ -43,20 +43,101 @@ in
 
   services.timesyncd.enable = true;
 
-  environment.etc."tailscale-auth-key".text = secrets.tailscale.authKey;
+  systemd.services.tailscaled-generate-auth-key = {
+    description = "Generate a tailscale auth key if necessary";
 
-  services.tailscale = {
+    before = [ "tailscaled-autoconnect.service" ];
+    wantedBy = [ "tailscaled-autoconnect.service" ];
+    after = [
+      "tailescaled.service"
+    ];
+    wants = [
+      "tailescaled.service"
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+
+    path = with pkgs; [
+      tailscale
+      jq
+      curl
+    ];
+    script = ''
+      wait_for_time() {
+        local i=0
+
+        while (( i < 60 )); do
+          local year
+          year=$(date +%Y)
+
+          if (( year > 2000 )); then
+            return 0
+          fi
+
+          sleep 1
+          ((i++))
+        done
+
+        echo "Error: Time did not sync" >&2
+        exit 1
+      }
+
+      wait_for_time
+
+      state="$(tailscale status --json --peers=false | jq -r '.BackendState')"
+
+      # No need to do anything if we're already up
+      if [[ ! "$state" =~ ^(NeedsLogin|NeedsMachineAuth|Stopped)$ ]]; then
+        echo "Tailscale is already authenticated. Nothing to do."
+        exit 0
+      fi
+
+      accessToken="$(curl --request POST \
+        --url https://api.tailscale.com/api/v2/oauth/token \
+        --header "Authorization: Basic $(echo -n "${secrets.tailscale.clientID}:${secrets.tailscale.clientSecret}" | base64 -w 0)" \
+        --header 'content-type: application/x-www-form-urlencoded' \
+        --data grant_type=client_credentials \
+        --data scope=auth_keys | jq -r '.access_token')"
+
+      deviceKey="$(curl 'https://api.tailscale.com/api/v2/tailnet/${secrets.tailscale.tailnet}/keys' \
+        --request POST \
+        --header 'Content-Type: application/json' \
+        --header "Authorization: Bearer $accessToken" \
+        --data '{
+        "keyType": "auth",
+        "description": "${systemName}",
+        "capabilities": {
+          "devices": {
+            "create": {
+              "reusable": false,
+              "ephemeral": false,
+              "preauthorized": false,
+              "tags": [
+                "tag:${systemSecrets.tailscale.locationTag}",
+                "tag:pi-hole"
+              ]
+            }
+          }
+        },
+        "expirySeconds": 60
+      }' | jq -r '.key')"
+
+      echo "$deviceKey" > /etc/tailnet-auth-key
+      echo "Wrote device key to /etc/tailnet-auth-key"
+    '';
+  };
+
+  services.tailscale = rec {
     enable = true;
     openFirewall = true;
-    authKeyFile = "/etc/tailscale-auth-key";
+    authKeyFile = "/etc/tailnet-auth-key";
     useRoutingFeatures = "server";
-    extraUpFlags = [
-      "--advertise-tags=tag:${systemSecrets.tailscale.locationTag}"
-    ];
     extraSetFlags = [
       "--advertise-exit-node"
       "--advertise-routes=${lib.concatStringsSep "," systemSecrets.tailscale.accessibleSubnets}"
     ];
+    extraUpFlags = extraSetFlags; # Without this, you can't use up after set runs
   };
 
   services.pihole-ftl = {
